05f5702e2664401d960c302cccc1bc3ac2b344d0
925176758@qq.com <925176758@qq.com>
2019-05-16 17:48:02 +0800

    jiuhua
    2019/5/16

---
+++ App.vue
@@ -0,0 +1,37 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	uni-tabbar{
+		height:98upx;
+		background:rgba(255,255,255,1);
+		box-shadow:0px 0px 13upx 0px rgba(217, 217, 217, 0.35);
+	}
+	.uni-tabbar__item:nth-child(2) .uni-tabbar__icon{
+		width: 42upx;
+		height: 35upx;
+	}
+	.uni-tabbar__item:nth-child(3) .uni-tabbar__icon{
+		width: 42upx;
+		height: 40upx;
+	}
+	.uni-tabbar__item:nth-child(4) .uni-tabbar__icon{
+		width: 44upx;
+		height: 40upx;
+	}
+	uni-tabbar .uni-tabbar__label{
+		font-size: 24upx !important;
+	}
+</style>
---
+++ components/topSearch.vue
@@ -0,0 +1,88 @@
+<template nane="topSearch">
+	<view class="top_search">
+		<view class="top_search_map"><text>{{title}}</text><image src="../static/index/topSearch_icon.png"></image></view>
+		<image class="top_search_icon" src="../static/index/topSearch_s.png"></image>
+		<input confirmtype="text" placeholder="输入意向位置/业态">
+		<view class="top_search_m"><image src="../static/index/topSearch_m.png"></image></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "topSearch",
+		props:{
+			title:{
+				type: String,
+				default: ""
+			}
+		},
+		data() {
+			return {
+				
+			}
+		},
+		created(){
+			
+		},
+		mounted(){
+			
+		},
+		methods: {
+			
+		}
+	}	
+</script>
+
+<style>
+	.top_search{
+		width: 497upx;
+		padding: 10upx 87upx 10upx 166upx;
+		position: relative;
+	}
+	.top_search input{
+		width:447upx;
+		height:60upx;
+		background:rgba(255,255,255,1);
+		box-shadow:0px 0px 7upx 0px rgba(191, 191, 191, 0.35);
+		border-radius:10upx;
+		font-size:24upx;
+		text-align: center;
+		color:rgba(153,153,153,1);
+		line-height: 60upx;
+		padding-left: 50upx;
+	}
+	.top_search_map{
+		position: absolute;
+		top: 10upx;
+		left: 26upx;
+		line-height: 60upx;
+	}
+	.top_search_map text{
+		font-size: 30upx;
+		color:rgba(51,51,51,1);
+		margin-right: 8upx;
+	}
+	.top_search_map image{
+		width: 17upx;
+		height: 9upx;
+		display: inline-block;
+		vertical-align: 10upx;
+	}
+	.top_search_m{
+		position: absolute;
+		top: 10upx;
+		right: 17upx;
+		line-height: 60upx;
+	}
+	.top_search_m image{
+		width: 26upx;
+		height: 28upx;
+	}
+	.top_search_icon{
+		width: 32upx;
+		height: 32upx;
+		position: absolute;
+		top: 24upx;
+		left: 38.4%;
+	}
+</style>
